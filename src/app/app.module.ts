6b1b0d42d5bf7a7bd47bccd01e5f577e28e0d67a
shustriy <a.k.kotelnikov@gmail.com>
2017-10-29 21:51:49 +0200

    host and host-context

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { DetailsComponent } from './hero/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    DetailsComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/hero/details/details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'details1',
+    templateUrl: './details.component.html',
+    styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+    title = 'details';
+}
