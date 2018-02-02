c0e31287856ca688b6754bf522a5863182e3b6ac
Andrey Kotelnikov <andrey.kotelnikov@gameloft.com>
2018-02-02 18:59:09 +0200

    temporary commit

---
+++ src/app/services/counter.actions.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const INCREMENT_ODD = 'INCREMENT_ODD';
+export const INCREMENT_ASYNC = 'INCREMENT_ASYNC';
+
+@Injectable()
+export class CounterActions {
+}
