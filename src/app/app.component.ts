051aadc4c5ffbecea0c080a342be3a63aafd943d
shustriy <a.k.kotelnikov@gmail.com>
2018-02-25 11:51:58 +0200

    Template reference variables AND fromEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import * as Rx from 'rxjs/Rx';
 
 @Component({
@@ -6,9 +6,10 @@ import * as Rx from 'rxjs/Rx';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   public title: string = 'app';
   public cnt: number = 0;
+  @ViewChild('btn') button: ElementRef;
 
   public ngOnInit() {
     console.log('ngOnInit');
@@ -32,7 +33,14 @@ export class AppComponent implements OnInit {
     //    .subscribe((x) => console.log('x', x));
 
   }
-  public onClick() {
-
+  public ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    const btnObservable = Rx.Observable
+            .fromEvent(this.button.nativeElement, 'click')
+            .switchMap(() => Rx.Observable
+              .of(4,2,3,8)
+              .map(val => ',' + val)
+            )
+            .subscribe((x) => console.log('x', x));
   }
 }
