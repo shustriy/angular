44581a2d7daf6724a1cf3803ad87c271d34eeea4
shustriy <a.k.kotelnikov@gmail.com>
2018-02-25 14:01:46 +0200

    Subject as Observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,36 +13,102 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   public ngOnInit() {
     console.log('ngOnInit');
-    //Rx.Observable
-    //    .of(1,2,3)
-    //    .map(x => x + '&')
-    //    .subscibe((x) => console.log('x:', x));
 
+    //this.runObservableMap();
+    //this.runObservabeMapWithTake();
+    //this.runObservableSwitchMap();
+    //this.runSubjectFromGuide();
+    this.runSubjectObservable();
+  }
+
+  public ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    //this.runObservableFromEvent();
+  }
+
+  public runObservableMap() {
+    Rx.Observable
+        .of(1,2,3)
+        .map(x => x + '&')
+        .subscribe((x) => console.log('x:', x));
+  }
+
+  public runObservabeMapWithTake()
+  {
+    Rx.Observable
+        .interval(200)
+        .take(9)
+        .map(x => x + "!!!")
+        .bufferCount(2)
+        .subscribe((x) => console.log('x', x));
+  }
+
+  public runObservableSwitchMap() {
     Rx.Observable.of(1,3,4,5,6,7,8)
         .switchMap((x) => Rx.Observable
             .of(4,2,3,8)
             .map(val => x + ',' + val)
         )
         .subscribe((x) => console.log(x));
-
-    //Rx.Observable
-    //    .interval(200)
-    //    .take(9)
-    //    .map(x => x + "!!!")
-    //    .bufferCount(2)
-    //    .subscribe((x) => console.log('x', x));
-
   }
-  public ngAfterViewInit() {
-    console.log('ngAfterViewInit');
+
+  public runObservableFromEvent() {
     const btnObservable = Rx.Observable
-            .fromEvent(this.button.nativeElement, 'click')
-            .switchMap(() => Rx.Observable
-              .of(4,2,3,8)
-              .map(val => ',' + val)
-            );
+        .fromEvent(this.button.nativeElement, 'click')
+        .switchMap(() => Rx.Observable
+            .of(4,2,3,8)
+            .map(val => ',' + val)
+        );
     const btnSubscriber = btnObservable.subscribe((x) => console.log('x', x));
     console.log('btnObservable', btnObservable);
     console.log('btnSubscriber', btnSubscriber);
+  }
+
+
+  public runSubjectFromGuide() {
+    var source = Rx.Observable.interval(1000);
+
+    var subject = new Rx.Subject();
+
+    var subSource = source.subscribe(subject);
+
+    var subSubject1 = subject.subscribe(
+        function (x) { console.log('Value published to observer #1: ' + x); },
+        function (e) { console.log('onError: ' + e.message); },
+        function () { console.log('onCompleted'); });
+
+    var subSubject2 = subject.subscribe(
+        function (x) { console.log('Value published to observer #2: ' + x); },
+        function (e) { console.log('onError: ' + e.message); },
+        function () { console.log('onCompleted'); });
+
+    setTimeout(function () {
+      // Clean up
+      subject.complete();
+    }, 5000);
+  }
+
+  public runSubjectObservable() {
+    var source = Rx.Observable.from([0,1,2,3,4,5,6,7,8,9]).delay(1000).map((x) => x*10);
+
+    var subject = new Rx.Subject();
+
+    var subSource = source.subscribe(subject);
+
+    var subSubject1 = subject.subscribe(
+        function (x) { console.log('Value published to observer #1: ' + x); },
+        function (e) { console.log('onError: ' + e.message); },
+        function () { console.log('onCompleted'); });
+
+    var subSubject2 = subject.subscribe(
+        function (x) { console.log('Value published to observer #2: ' + x); },
+        function (e) { console.log('onError: ' + e.message); },
+        function () { console.log('onCompleted'); });
+    console.log('source', source, 'subSubject1', subSubject1, 'subSource', subSource);
+
+    setTimeout(function () {
+      // Clean up
+      subject.complete();
+    }, 5000);
   }
 }
